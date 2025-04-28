957b2de78b22a7ac4edb1d1d95bd1f20ea9108e8
JiHwanLee <kdgosu53@gmail.com>
2025-04-28 15:42:47 +0900

    shared/ui/index.tsx 파일 내 셀렉트 컴포넌트 분리

--- src/shared/ui/card/index.ts
+++ src/shared/ui/card/index.ts
@@ -1,2 +1,2 @@
-export { Card, CardHeader, CardTitle, CardContent } from "./card"
+export { Card, CardHeader, CardTitle, CardContent } from "./Card"
 export type { CardProps, CardHeaderProps, CardTitleProps, CardContentProps } from "./types"
---
+++ src/shared/ui/select/index.ts
@@ -0,0 +1,3 @@
+export { Select, SelectGroup, SelectValue, SelectTrigger, SelectContent, SelectItem } from "./Select"
+
+export type { SelectTriggerProps, SelectContentProps, SelectItemProps } from "./types"
